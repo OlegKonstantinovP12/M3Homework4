90a5a8782a4176dbd9e44c4f89797a63719b2c5e
Oleg Konstantinov <oleg.boss@gmail.com>
2025-12-29 07:25:51 +0100

    Initial Commit

---
+++ M3Homework4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M3Homework4
+//
+//  Created by Oleg Konstantinov on 29.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M3Homework4/M3Homework4App.swift
@@ -0,0 +1,17 @@
+//
+//  M3Homework4App.swift
+//  M3Homework4
+//
+//  Created by Oleg Konstantinov on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct M3Homework4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
